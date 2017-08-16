23739ea6d92584be62f69dc187d245a1d1e80c08
Joe Traynor <joseph.traynor@o2.com>
2017-08-16 22:38:45 +0100

    add select function and tweak manifest. state is now updated when a user clicks on each 'checkbox'

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ class Form extends Component {
     checkbox2: false,
     checkbox3: false
   }
+  
+  select = (key) => {
+    var checkboxState = this.state;
+    if (checkboxState[key] === false) {
+      this.setState({[key]: true});
+      return;
+    }
+    this.setState({[key]: false});
+  }
+
   render() {
     return (
       <div className="outerDiv">
         <h1 className="App-header">Checkbox Test</h1>
-        <form className="App">
-          <p>Checkbox</p>
+        <form onSubmit={this.submit} className="App">
+          {this.props.label.map((name) => {
+            return <p onClick={() => this.select(name)} key={name}>{name}</p>
+          })}
           <button>Submit</button>
         </form>
       </div>
