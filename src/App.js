2fe0e773f689d2702e0bb39fb42b45ea42299adb
Joe Traynor <joseph.traynor@o2.com>
2017-08-16 22:40:10 +0100

    added submit logging functionality to the console and updated cursor to look like a pointer. will not have time to style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Form extends Component {
+  
   state = {
     checkbox1: false,
     checkbox2: false,
     checkbox3: false
   }
-  
+  submit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
   select = (key) => {
     var checkboxState = this.state;
     if (checkboxState[key] === false) {
@@ -16,14 +20,16 @@ class Form extends Component {
     }
     this.setState({[key]: false});
   }
-
+  cursor = {
+    cursor:'pointer'
+  }
   render() {
     return (
       <div className="outerDiv">
         <h1 className="App-header">Checkbox Test</h1>
         <form onSubmit={this.submit} className="App">
           {this.props.label.map((name) => {
-            return <p onClick={() => this.select(name)} key={name}>{name}</p>
+            return <p style={this.cursor} className={this.state.class} onClick={() => this.select(name)} key={name}>{name}</p>
           })}
           <button>Submit</button>
         </form>
