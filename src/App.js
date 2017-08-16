4032be709edb956a683c5a86a4c1ed630b4187ac
Joe Traynor <joseph.traynor@o2.com>
2017-08-16 22:33:32 +0100

    renamed App to Form, and added props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Form extends Component {
   state = {
     checkbox1: false,
     checkbox2: false,
@@ -20,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Form;
